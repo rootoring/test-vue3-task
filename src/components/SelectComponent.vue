a8edb6d6be370fb5598706f066e4362fa60ef511
rootoring <laraptxe@gmail.com>
2024-09-03 20:46:03 +0300

    first

---
+++ src/components/SelectComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-select
+      :items="items"
+      v-model="selectedOption"
+      item-title="name"
+      item-value="id"
+      label="Выберите значение"
+      outlined
+      dense
+      variant="solo"
+    ></v-select>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+const emit = defineEmits(["update:modelValue"]);
+const selectedOption = ref(1);
+watch(selectedOption, (newVal) => {
+  emit("update:modelValue", newVal);
+});
+
+const items = ref([
+  { id: 1, name: "Без сортировки" },
+  { id: 2, name: "По возрастанию" },
+  { id: 3, name: "По убыванию" },
+]);
+</script>
+
+<style scoped></style>
